581e3f1812d63ff0f38b7c3d6f38d32a3f0903db
Colin Clark <colinbdclark@gmail.com>
2015-08-03 12:47:54 -0400

    Version 0.9.1

--- dist/bergson-only.js
+++ dist/bergson-only.js
@@ -1,4 +1,4 @@
-/*! Bergson 0.9.0, Copyright 2015 Colin Clark | github.com/colinbdclark/bergson */
+/*! Bergson 0.9.1, Copyright 2015 Colin Clark | github.com/colinbdclark/bergson */
 
 /*
  * Bergson Clocks
